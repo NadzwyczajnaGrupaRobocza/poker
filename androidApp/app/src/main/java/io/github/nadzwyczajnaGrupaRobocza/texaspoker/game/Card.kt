c32091bc6fa9b22fc8a92bb86252808d380cbf94
Tomasz Drzewiecki <199537+kn65op@users.noreply.github.com>
2020-06-27 09:20:37 +0200

    Rename package to start with lower case

---
+++ androidApp/app/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/Card.kt
@@ -0,0 +1,2 @@
+package io.github.nadzwyczajnaGrupaRobocza.texaspoker.game
+
--- androidApp/app/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/ui/slideshow/SlideshowViewModel.kt
+++ androidApp/app/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/ui/slideshow/SlideshowViewModel.kt
@@ -1,13 +1,13 @@
-package io.github.NadzwyczajnaGrupaRobocza.texaspoker.ui.gallery
+package io.github.nadzwyczajnaGrupaRobocza.texaspoker.ui.slideshow
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class SlideshowViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is slideshow Fragment"
     }
     val text: LiveData<String> = _text
 }
