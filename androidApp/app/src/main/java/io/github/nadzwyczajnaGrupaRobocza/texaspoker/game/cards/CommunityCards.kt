31f1767deb823b50fb24e256d554201541cc0c19
kn65op <199537+kn65op@users.noreply.github.com>
2020-07-06 19:51:04 +0200

    Add flop

--- androidApp/app/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/cards/CommunityCards.kt
+++ androidApp/app/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/cards/CommunityCards.kt
@@ -1,12 +1,16 @@
 package io.github.nadzwyczajnaGrupaRobocza.texaspoker.game.cards
 
-class CommunityCards() {
-    private constructor(flop1: Card, flop2: Card, flop3: Card) : this() {
-        this.size = 3
-    }
-    fun flop(flop1: Card, flop2: Card, flop3: Card): CommunityCards {
-        return CommunityCards(flop1, flop2, flop3)
+
+class NoCommunityCards {
+    fun flop(flop1: Card, flop2: Card, flop3: Card): FlopCommunityCards {
+        return FlopCommunityCards(this, flop1, flop2, flop3)
     }
 
-    var size = 0
+    val size = 0
+
+
+}
+
+class FlopCommunityCards(communityCards: NoCommunityCards, flop1: Card, flop2: Card, flop3: Card) {
+    val size = 3
 }
--- androidApp/app/src/test/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/cards/CommunityCardsTest.kt
+++ androidApp/app/src/test/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/cards/CommunityCardsTest.kt
@@ -4,24 +4,21 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.Test
 
-class CommunityCardsTest
-{
+class CommunityCardsTest {
     @Test
-    fun `Community cards should being with no cards`()
-    {
+    fun `Community cards should being with no cards`() {
         val noCards = 0
-        assertThat(CommunityCards().size , equalTo(noCards))
+        assertThat(NoCommunityCards().size, equalTo(noCards))
     }
 
     @Test
-    fun `After flop community cards shuold have size of flop`()
-    {
+    fun `After flop community cards shuold have size of flop`() {
         val cardsAfterFlop = 3
         val flop1 = Card(Suit.Clubs, Rank.Ten)
         val flop2 = Card(Suit.Spades, Rank.Ten)
         val flop3 = Card(Suit.Clubs, Rank.King)
 
-        assertThat(CommunityCards().flop(flop1, flop2, flop3).size, equalTo(cardsAfterFlop))
+        assertThat(NoCommunityCards().flop(flop1, flop2, flop3).size, equalTo(cardsAfterFlop))
     }
 
 }
