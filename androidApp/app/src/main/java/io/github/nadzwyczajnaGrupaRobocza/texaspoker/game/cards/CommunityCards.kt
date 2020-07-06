111ff9a3bfe178f9c46997288422c1bba84584d1
kn65op <199537+kn65op@users.noreply.github.com>
2020-07-06 19:52:02 +0200

    Add interface

--- androidApp/app/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/cards/CommunityCards.kt
+++ androidApp/app/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/cards/CommunityCards.kt
@@ -1,16 +1,18 @@
 package io.github.nadzwyczajnaGrupaRobocza.texaspoker.game.cards
 
+interface CommunityCards {
+    val size: Int
+}
 
-class NoCommunityCards {
+class NoCommunityCards : CommunityCards {
     fun flop(flop1: Card, flop2: Card, flop3: Card): FlopCommunityCards {
         return FlopCommunityCards(this, flop1, flop2, flop3)
     }
 
-    val size = 0
-
-
+    override val size = 0
 }
 
-class FlopCommunityCards(communityCards: NoCommunityCards, flop1: Card, flop2: Card, flop3: Card) {
-    val size = 3
+class FlopCommunityCards(communityCards: NoCommunityCards, flop1: Card, flop2: Card, flop3: Card) :
+    CommunityCards {
+    override val size = 3
 }
