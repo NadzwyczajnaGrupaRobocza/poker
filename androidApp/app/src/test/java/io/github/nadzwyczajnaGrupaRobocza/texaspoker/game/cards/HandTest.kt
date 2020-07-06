acdd081ac073a0df788c050ca2028daf29b4536e
kn65op <199537+kn65op@users.noreply.github.com>
2020-07-06 20:42:13 +0200

    Refactor

--- androidApp/app/src/test/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/cards/HandTest.kt
+++ androidApp/app/src/test/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/cards/HandTest.kt
@@ -5,15 +5,21 @@ import com.natpryce.hamkrest.equalTo
 import org.junit.Test
 
 class HandTest {
-    @Test
-    fun `Hand should be contructred from PockedCards and RiverCommunityCards`() {
-        val flop1 = Card(Suit.Diamonds, Rank.King)
-        val flop2 = Card(Suit.Spades, Rank.Queen)
-        val flop3 = Card(Suit.Diamonds, Rank.Ten)
-        val turnCard = Card(Suit.Hearts, Rank.Nine)
-        val riverCard = Card(Suit.Spades, Rank.Three)
-        val pocketCard1 = Card(Suit.Clubs, Rank.Four)
-        val pocketCard2 = Card(Suit.Clubs, Rank.Five)
+    private fun createPocketCards(
+        pocketCard1: Card,
+        pocketCard2: Card
+    ): PocketCards {
+        val pockerCards = PocketCards(pocketCard1, pocketCard2)
+        return pockerCards
+    }
+
+    private fun createRiver(
+        flop1: Card,
+        flop2: Card,
+        flop3: Card,
+        turnCard: Card,
+        riverCard: Card
+    ): RiverCommunityCards {
         val river = RiverCommunityCards(
             TurnCommunityCards(
                 FlopCommunityCards(
@@ -24,9 +30,24 @@ class HandTest {
                 ), turnCard
             ), riverCard
         )
-        val pockerCards = PocketCards(pocketCard1, pocketCard2)
+        return river
+    }
+
+    @Test
+    fun `Hand should be contructred from PockedCards and RiverCommunityCards`() {
+        val flop1 = Card(Suit.Diamonds, Rank.King)
+        val flop2 = Card(Suit.Spades, Rank.Queen)
+        val flop3 = Card(Suit.Diamonds, Rank.Ten)
+        val turnCard = Card(Suit.Hearts, Rank.Nine)
+        val riverCard = Card(Suit.Spades, Rank.Three)
+        val pocketCard1 = Card(Suit.Clubs, Rank.Four)
+        val pocketCard2 = Card(Suit.Clubs, Rank.Five)
+
+        val river = createRiver(flop1, flop2, flop3, turnCard, riverCard)
+        val pockerCards = createPocketCards(pocketCard1, pocketCard2)
         val hand = Hand(river, pockerCards)
 
         assertThat(hand.cards, equalTo(setOf(flop1, flop2, flop3, riverCard, turnCard, pocketCard1, pocketCard2)))
     }
+
 }
