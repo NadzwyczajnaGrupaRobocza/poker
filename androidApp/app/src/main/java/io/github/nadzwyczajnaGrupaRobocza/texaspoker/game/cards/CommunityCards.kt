aeff7e297afffeb34f7891143daca022827070a4
kn65op <199537+kn65op@users.noreply.github.com>
2020-07-06 19:35:17 +0200

    Community cards starts with no cards

--- androidApp/app/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/cards/CommunityCards.kt
+++ androidApp/app/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/cards/CommunityCards.kt
@@ -1,5 +1,5 @@
 package io.github.nadzwyczajnaGrupaRobocza.texaspoker.game.cards
 
 class CommunityCards {
-
+    val size = 0
 }
--- androidApp/app/src/test/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/cards/CommunityCardsTest.kt
+++ androidApp/app/src/test/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/cards/CommunityCardsTest.kt
@@ -1,5 +1,7 @@
 package io.github.nadzwyczajnaGrupaRobocza.texaspoker.game.cards
 
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
 import org.junit.Test
 
 class CommunityCardsTest
@@ -7,7 +9,12 @@ class CommunityCardsTest
     @Test
     fun `Community cards should being with no cards`()
     {
-        CommunityCards()
+        val noCards = 0
+        assertThat(CommunityCards().size , equalTo(noCards))
     }
+
+    @Test
+    fun `Flop should be added to community cards`()
+    {}
 
 }
