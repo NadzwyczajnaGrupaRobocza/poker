4d01475699ebf4f29aefd1f47347f1470be3742e
Lukasz Maciejewski <lukasz.m.maciejewski@pm.me>
2020-07-12 11:36:39 +0200

    moving server data structures to separate module
    WIP, mess, etc
    also adding gitignore file for android studio build artifacts

--- session_server/src/main.rs
+++ session_server/src/main.rs
@@ -4,8 +4,6 @@
 extern crate rocket;
 #[macro_use]
 extern crate rocket_contrib;
-#[macro_use]
-extern crate serde_derive;
 
 use rocket::State;
 use std::collections::HashMap;
@@ -14,71 +12,11 @@ use std::sync::Mutex;
 use rocket_contrib::json::{Json, JsonValue};
 use uuid::Uuid;
 
-#[derive(PartialEq, Eq, Hash, Clone)]
-struct UserID {
-    user_id: String,
-}
+pub use session_server::db_types::db;
 
-impl UserID {
-    pub fn new(id: &String) -> UserID {
-        UserID {
-            user_id: id.clone(),
-        }
-    }
-}
-
-struct UserData {
-    known_peers: KnownPeers,
-    active_sessions: Vec<SessionID>,
-}
-
-impl UserData {
-    pub fn new() -> UserData {
-        UserData {
-            known_peers: KnownPeers::new(),
-            active_sessions: vec![],
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize)]
-struct KnownPeers {
-    peers: Vec<String>,
-}
-
-impl KnownPeers {
-    pub fn new() -> KnownPeers {
-        KnownPeers { peers: vec![] }
-    }
-}
-
-#[derive(PartialEq, Eq, Hash, Serialize, Deserialize, Clone)]
-struct SessionID {
-    session_id: String,
-}
-
-impl SessionID {
-    pub fn make_new() -> SessionID {
-        let id = Uuid::new_v4();
-        id.simple();
-        SessionID {
-            session_id: format!("{}", id.simple()),
-        }
-    }
-}
-
-struct PendingSessionData {
-    unconfirmed_participants: Vec<UserID>,
-    confirmed_participants: Vec<UserID>,
-}
-
-struct SessionData {
-    participants: Vec<UserID>,
-}
-
-type UsersDB = HashMap<UserID, UserData>;
-type SessionsDB = HashMap<SessionID, SessionData>;
-type PendingSessionDB = HashMap<SessionID, PendingSessionData>;
+type UsersDB = HashMap<db::UserID, db::UserData>;
+type SessionsDB = HashMap<db::SessionID, db::SessionData>;
+type PendingSessionDB = HashMap<db::SessionID, db::PendingSessionData>;
 
 struct ServerDB {
     users_db: UsersDB,
@@ -88,20 +26,15 @@ struct ServerDB {
 
 impl ServerDB {
     pub fn new() -> ServerDB {
-        ServerDB{ users_db: UsersDB::new(),
-                  session_db: SessionsDB::new(),
-                  pending_session_db: PendingSessionDB::new(),
+        ServerDB {
+            users_db: UsersDB::new(),
+            session_db: SessionsDB::new(),
+            pending_session_db: PendingSessionDB::new(),
         }
     }
 }
 
 type MutexServerDB = Mutex<ServerDB>;
-
-#[derive(Serialize,Deserialize)]
-struct CreateSessionReq {
-    creator_id: String,
-    other_participants: Vec<String>,
-}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -111,12 +44,15 @@ fn index() -> &'static str {
 #[put("/user/<id>/known_peers", format = "json", data = "<peers>")]
 fn update_known_peers(
     id: String,
-    peers: Json<KnownPeers>,
+    peers: Json<db::KnownPeers>,
     db: State<'_, MutexServerDB>,
 ) -> JsonValue {
     let mut db = db.lock().unwrap();
     println!("updated peers for {} to {:?}", id, peers.0.peers);
-    let users_entry = db.users_db.entry(UserID::new(&id)).or_insert(UserData::new());
+    let users_entry = db
+        .users_db
+        .entry(db::UserID::new(&id))
+        .or_insert(db::UserData::new());
     users_entry.known_peers.peers = peers.0.peers;
 
     json!({ "status": "ok" })
@@ -125,25 +61,27 @@ fn update_known_peers(
 #[get("/user/<id>/known_peers")]
 fn get_known_peers(id: String, db: State<'_, MutexServerDB>) -> JsonValue {
     let db = db.lock().unwrap();
-    if db.users_db.contains_key(&UserID::new(&id)) {
+    if db.users_db.contains_key(&db::UserID::new(&id)) {
         json!({ "status" : "ok", })
     } else {
         json!({ "status" : "not found" })
     }
 }
 
-#[put("/session/create", format="json", data = "<req>")]
-fn create_session(req: Json<CreateSessionReq>, db: State<'_, MutexServerDB>) -> JsonValue {
-    let session_id = SessionID::make_new();
+#[put("/session/create", format = "json", data = "<req>")]
+fn create_session(req: Json<db::CreateSessionReq>, db: State<'_, MutexServerDB>) -> JsonValue {
+    let session_id = db::SessionID::make_new();
     let mut db = db.lock().unwrap();
-    let session_data = PendingSessionData {
-        unconfirmed_participants: req.0.other_participants.iter().map(&UserID::new).collect(),
-        confirmed_participants: vec![UserID::new(&req.0.creator_id)] };
+    let session_data = db::PendingSessionData {
+        unconfirmed_participants: req.0.other_participants.iter().map(&db::UserID::new).collect(),
+        confirmed_participants: vec![db::UserID::new(&req.0.creator_id)],
+    };
     // Returns Option<V> which is empty if key is new and contains old value if key is already present.
     // Mayby a better add method should be used? Adding a check for session being empty would be nice.
     // https://doc.rust-lang.org/std/collections/struct.HashMap.html#method.insert
-    let _session = db.pending_session_db.insert(session_id.clone(), session_data);
-
+    let _session = db
+        .pending_session_db
+        .insert(session_id.clone(), session_data);
 
     json!({"status": "ok",
            "session_id": session_id })
@@ -152,7 +90,10 @@ fn create_session(req: Json<CreateSessionReq>, db: State<'_, MutexServerDB>) -> 
 fn main() {
     println!("rocket launch?");
     rocket::ignite()
-        .mount("/", routes![index, get_known_peers, update_known_peers, create_session])
+        .mount(
+            "/",
+            routes![index, get_known_peers, update_known_peers, create_session],
+        )
         .manage(Mutex::new(ServerDB::new()))
         .launch();
 }
