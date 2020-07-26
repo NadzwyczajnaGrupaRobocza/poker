539a4dabbfce6826078bfe1a5cc9bce029cc5fe6
kn65op <199537+kn65op@users.noreply.github.com>
2020-07-26 13:54:18 +0200

    Pair some work

--- androidApp/core/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/cards/Hand.kt
+++ androidApp/core/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/cards/Hand.kt
@@ -5,51 +5,116 @@ import java.lang.Integer.max
 class Hand(river: RiverCommunityCards, pocketCards: PocketCards) {
     val cards = river.cards + pocketCards.card1 + pocketCards.card2
 
-    val type: HandType = calculateHandType(cards)
+    private data class InternalHand(
+        val type: HandType,
+        val handTypeCards: List<Card>,
+        val kickers: List<Card>
+    )
 
-    enum class StraightType {
+    private val internalHand = calculateInternalHand(cards)
+
+    val type: HandType
+        get() = internalHand.type
+    val importantCards
+        get() = internalHand.handTypeCards
+    val kickers
+        get() = internalHand.kickers
+
+    private enum class StraightType {
         Normal,
         Flush,
         Royal
     }
 
-    private fun calculateHandType(cards: Set<Card>): HandType {
-        val pair = 2
-        val three = 3
-        val four = 4
+    private fun calculateInternalHand(cards: Set<Card>): InternalHand {
         val oneElement = 1
         val twoElements = 2
         val fiveElements = 5
 
-        val cardsByRank = cards.groupBy { it.rank }
-        val pairsCount = cardsByRank.count { it.value.size == pair }
-        val threesCount = cardsByRank.count { it.value.size == three }
-        val foursCount = cardsByRank.count { it.value.size == four }
+        val cardsByRank = groupCardsByRank(cards)
+        val (pairs, threes, fours) = calculateMultipleRanks(cardsByRank)
+        val maxCardsInOneSuite = calculateMaxCardsInOneSuit(cards)
+        val straightType = calculateStraight(cards)
 
-        val cardsBySuite = cards.groupBy { it.suit }
-        val maxCardsInOneSuite = cardsBySuite.maxBy { it.value.size }?.value?.size
+        if (straightType == StraightType.Royal) return InternalHand(
+            HandType.RoyalFlush,
+            emptyList(),
+            emptyList()
+        )
+        if (straightType == StraightType.Flush) return InternalHand(
+            HandType.StraightFlush,
+            emptyList(),
+            emptyList()
+        )
+        if (fours.size == oneElement) return InternalHand(HandType.Four, emptyList(), emptyList())
+        if (threes.size == oneElement && pairs.size >= oneElement) return InternalHand(
+            HandType.Full,
+            emptyList(),
+            emptyList()
+        )
+        maxCardsInOneSuite?.let {
+            if (maxCardsInOneSuite >= fiveElements) return InternalHand(
+                HandType.Flush,
+                emptyList(),
+                emptyList()
+            )
+        }
+        if (straightType == StraightType.Normal) return InternalHand(
+            HandType.Straight,
+            emptyList(),
+            emptyList()
+        )
+        if (threes.size == oneElement) return InternalHand(HandType.Three, emptyList(), emptyList())
+        if (pairs.size == oneElement) {
+            val pairRank = pairs.first().first().rank
+            return InternalHand(
+                HandType.Pair,
+                cards.filter { it.rank == pairRank },
+                cards.filter { it.rank != pairRank }.sortedByDescending { it.rank }.take(3)
+            )
+        }
+        if (pairs.size >= twoElements) return InternalHand(
+            HandType.TwoPairs,
+            emptyList(),
+            emptyList()
+        )
 
-        val straightType = isStraight(cards)
-
-        if (straightType == StraightType.Royal) return HandType.RoyalFlush
-        if (straightType == StraightType.Flush) return HandType.StraightFlush
-        if (foursCount == oneElement) return HandType.Four
-        if (threesCount == oneElement && pairsCount >= oneElement) return HandType.Full
-        maxCardsInOneSuite?.let { if (maxCardsInOneSuite >= fiveElements) return HandType.Flush }
-        if (straightType == StraightType.Normal) return HandType.Straight
-        if (threesCount == oneElement) return HandType.Three
-        if (pairsCount == oneElement) return HandType.Pair
-        if (pairsCount >= twoElements) return HandType.TwoPairs
-
-        return HandType.HighCard
+        return InternalHand(
+            HandType.HighCard,
+            cards.sortedByDescending { it.rank }.take(5),
+            emptyList()
+        )
     }
 
-    private fun isStraight(cards: Set<Card>): StraightType? {
+    private fun calculateMaxCardsInOneSuit(cards: Set<Card>): Int? {
+        val cardsBySuite = groupCardsBySuit(cards)
+        return cardsBySuite.maxBy { it.value.size }?.value?.size
+    }
+
+    private fun calculateMultipleRanks(cardsByRank: Map<Rank, List<Card>>): Triple<List<List<Card>>, List<List<Card>>, List<List<Card>>> {
+        val pair = 2
+        val three = 3
+        val four = 4
+        val pairs = cardsByRank.filter { it.value.size == pair }.map { it.value }
+        val threes = cardsByRank.filter { it.value.size == three }.map { it.value }
+        val fours = cardsByRank.filter { it.value.size == four }.map { it.value }
+        return Triple(pairs, threes, fours)
+    }
+
+    private fun groupCardsBySuit(cards: Set<Card>): Map<Suit, List<Card>> {
+        return cards.groupBy { it.suit }
+    }
+
+    private fun groupCardsByRank(cards: Set<Card>): Map<Rank, List<Card>> {
+        return cards.groupBy { it.rank }
+    }
+
+    private fun calculateStraight(cards: Set<Card>): StraightType? {
         val cardsSortedByRank = cards.sortedBy { it.rank }
-        return isStraight(cards = cardsSortedByRank, previousCard = cardsSortedByRank.last())
+        return calculateStraight(cards = cardsSortedByRank, previousCard = cardsSortedByRank.last())
     }
 
-    private fun isStraight(
+    private fun calculateStraight(
         cards: List<Card>,
         previousCard: Card,
         cardsInRow: Int = 0,
@@ -90,7 +155,7 @@ class Hand(river: RiverCommunityCards, pocketCards: PocketCards) {
         suites: List<Suit>
     ): StraightType? {
         if (diff == 1 || diff == -12)
-            return isStraight(
+            return calculateStraight(
                 cards.subList(1, cards.size),
                 thisCard,
                 max(cardsInRow + 1, 2),
@@ -98,14 +163,14 @@ class Hand(river: RiverCommunityCards, pocketCards: PocketCards) {
                 (suites + thisCard.suit + if (cardsInRow == 0) previousCard.suit else null).filterNotNull()
             )
         if (diff == 0)
-            return isStraight(
+            return calculateStraight(
                 cards.subList(1, cards.size),
                 thisCard,
                 cardsInRow,
                 newMax,
                 (suites + thisCard.suit + if (cardsInRow == 0) previousCard.suit else null).filterNotNull()
             )
-        return isStraight(
+        return calculateStraight(
             cards = cards.subList(1, cards.size),
             previousCard = thisCard,
             maxCardsInRow = newMax,
--- androidApp/core/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/cards/HandCompare.kt
+++ androidApp/core/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/cards/HandCompare.kt
@@ -1,32 +1,30 @@
+import io.github.nadzwyczajnaGrupaRobocza.texaspoker.game.cards.Card
 import io.github.nadzwyczajnaGrupaRobocza.texaspoker.game.cards.Hand
-import io.github.nadzwyczajnaGrupaRobocza.texaspoker.game.cards.HandType
 import io.github.nadzwyczajnaGrupaRobocza.texaspoker.game.cards.Rank
 
 private data class RanksToCompare(val lhs: Rank, val rhs: Rank)
 
-private val equal = 0
-private val bigger = 1
-private val less = -1
+private const val equal = 0
+private const val bigger = 1
+private const val less = -1
 
 fun Hand.compareTo(other: Hand): Int =
     when {
         this.type < other.type -> less
         this.type > other.type -> bigger
-        this.type == HandType.HighCard -> compareHighCards(this, other)
-        this.type == HandType.Pair -> comparePairs(this, other)
-        else -> equal
+        else -> compareFirstImportantCardsThenKickers(this, other)
     }
 
-fun compareHighCards(lhs: Hand, rhs: Hand): Int {
-    val lhsCards = lhs.cards.sortedByDescending { it.rank }.take(5)
-    val rhsCards = rhs.cards.sortedByDescending { it.rank }.take(5)
-    val rankPairs =
-        lhsCards.mapIndexed { index, card -> RanksToCompare(card.rank, rhsCards[index].rank) }
-    return compareToFirstNotEqual(rankPairs)
-}
+fun compareFirstImportantCardsThenKickers(lhs: Hand, rhs: Hand) =
+    when (val it = compareCards(lhs.importantCards, rhs.importantCards)) {
+        equal -> compareCards(lhs.kickers, rhs.kickers)
+        else -> it
+    }
 
-fun comparePairs(lhs: Hand, rhs: Hand): Int {
-    return equal
+fun compareCards(lhs: List<Card>, rhs: List<Card>): Int {
+    val rankPairs =
+        lhs.mapIndexed { index, card -> RanksToCompare(card.rank, rhs[index].rank) }
+    return compareToFirstNotEqual(rankPairs)
 }
 
 private fun compareToFirstNotEqual(ranks: List<RanksToCompare>): Int = when {
