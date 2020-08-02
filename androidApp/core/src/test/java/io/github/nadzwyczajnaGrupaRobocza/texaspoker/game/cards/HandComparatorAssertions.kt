4e235d0a0166ef1545fad16c8fa41964b5344a38
kn65op <199537+kn65op@users.noreply.github.com>
2020-08-02 17:45:10 +0200

    Extract to files

---
+++ androidApp/core/src/test/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/cards/HandComparatorAssertions.kt
@@ -0,0 +1,28 @@
+package io.github.nadzwyczajnaGrupaRobocza.texaspoker.game.cards
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import com.natpryce.hamkrest.greaterThan
+import com.natpryce.hamkrest.lessThan
+import compareTo
+
+fun assertHandsEqual(
+    lhs: Hand,
+    rhs: Hand
+) {
+    assertThat(lhs.compareTo(rhs), equalTo(0))
+}
+
+fun assertHandBigger(
+    lhs: Hand,
+    rhs: Hand
+) {
+    assertThat(lhs.compareTo(rhs), greaterThan(0))
+}
+
+fun assertHandLess(
+    lhs: Hand,
+    rhs: Hand
+) {
+    assertThat(lhs.compareTo(rhs), lessThan(0))
+}
---
+++ androidApp/core/src/test/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/cards/HighCardsComparatorTest.kt
@@ -0,0 +1,151 @@
+package io.github.nadzwyczajnaGrupaRobocza.texaspoker.game.cards
+
+import org.junit.Test
+
+class HighCardsComparatorTest {
+    @Test
+    fun `HighCards should be equal when only differ in suit`() {
+        val lhs =
+            createHand(
+                clubsFive,
+                clubsFour,
+                spadesThree,
+                heartsNine,
+                diamondsTen,
+                spadesQueen,
+                diamondsKing
+            )
+        val rhs =
+            createHand(
+                diamondsFive,
+                spadesFour,
+                diamondsThree,
+                spadesNine,
+                clubsTen,
+                diamondsQueen,
+                spadesKing
+            )
+
+        assertHandsEqual(
+            lhs,
+            rhs
+        )
+    }
+
+    @Test
+    fun `HighCard should be bigger when first card is bigger`() {
+        val lhs =
+            createHand(
+                clubsFive,
+                clubsFour,
+                spadesThree,
+                heartsNine,
+                diamondsTen,
+                spadesQueen,
+                diamondsAce
+            )
+        val rhs =
+            createHand(
+                diamondsFive,
+                spadesFour,
+                diamondsThree,
+                spadesNine,
+                clubsTen,
+                diamondsQueen,
+                spadesKing
+            )
+
+        assertHandBigger(
+            lhs,
+            rhs
+        )
+    }
+
+    @Test
+    fun `HighCard should be less when second card is less`() {
+        val lhs =
+            createHand(
+                clubsFive,
+                clubsFour,
+                spadesThree,
+                heartsNine,
+                diamondsTen,
+                spadesJack,
+                diamondsKing
+            )
+        val rhs =
+            createHand(
+                diamondsFive,
+                spadesFour,
+                diamondsThree,
+                spadesNine,
+                clubsTen,
+                diamondsQueen,
+                spadesKing
+            )
+
+        assertHandLess(
+            lhs,
+            rhs
+        )
+    }
+
+    @Test
+    fun `HighCard should be bigger when fifth card is less`() {
+        val lhs =
+            createHand(
+                diamondsSix,
+                clubsFour,
+                spadesThree,
+                heartsNine,
+                diamondsTen,
+                spadesQueen,
+                diamondsKing
+            )
+        val rhs =
+            createHand(
+                diamondsFive,
+                spadesFour,
+                diamondsThree,
+                spadesNine,
+                clubsTen,
+                diamondsQueen,
+                spadesKing
+            )
+
+        assertHandBigger(
+            lhs,
+            rhs
+        )
+    }
+
+
+    @Test
+    fun `HighCard should be qual when sixth card is bigger`() {
+        val lhs =
+            createHand(
+                clubsFive,
+                clubsFour,
+                spadesThree,
+                heartsNine,
+                diamondsTen,
+                spadesQueen,
+                diamondsKing
+            )
+        val rhs =
+            createHand(
+                diamondsFive,
+                spadesTwo,
+                diamondsThree,
+                spadesNine,
+                clubsTen,
+                diamondsQueen,
+                spadesKing
+            )
+
+        assertHandsEqual(
+            lhs,
+            rhs
+        )
+    }
+}
