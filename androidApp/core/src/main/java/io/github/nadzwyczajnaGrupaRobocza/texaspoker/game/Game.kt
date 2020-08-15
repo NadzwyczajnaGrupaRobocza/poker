1bbfc69d12d1b16fdc74690ef8f524dcee88be9f
kn65op <199537+kn65op@users.noreply.github.com>
2020-08-15 20:59:59 +0200

    Add activePlayers property

--- androidApp/core/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/Game.kt
+++ androidApp/core/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/Game.kt
@@ -1,11 +1,22 @@
 package io.github.nadzwyczajnaGrupaRobocza.texaspoker.game
 
-class Game(val players: List<Player>) {
+class Game(players: List<Player>, startingChips: Int) {
+    val activePlayers: Set<Player>
+        get() = players.filter { it.chips > 0 }.toExternal().toSet()
+
+    private val players = players.toInternal(startingChips)
+
     init {
         if (players.size > 8 || players.size < 2) throw InvalidPlayersNumber(players.size)
     }
 
 }
+
+private class InternalPlayer(val player: Player, val chips: Int)
+
+private fun List<Player>.toInternal(startingChips: Int) = map { InternalPlayer(it, startingChips) }
+
+private fun List<InternalPlayer>.toExternal() = map { it.player }
 
 class InvalidPlayersNumber(val size: Int) : Throwable() {
     override fun equals(other: Any?): Boolean {
@@ -23,3 +34,5 @@ class InvalidPlayersNumber(val size: Int) : Throwable() {
         return size
     }
 }
+
+
--- androidApp/core/src/test/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/GameTest.kt
+++ androidApp/core/src/test/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/GameTest.kt
@@ -6,29 +6,34 @@ import com.natpryce.hamkrest.throws
 import org.junit.Test
 
 class GameTest {
-    val player1= Player("1")
-    val player2= Player("2")
-    val player3= Player("3")
-    val player4= Player("4")
-    val player5= Player("5")
-    val player6= Player("6")
-    val player7= Player("7")
-    val player8= Player("8")
-    val player9= Player("9")
+    private val player1 = Player("1")
+    private val player2 = Player("2")
+    private val player3 = Player("3")
+    private val player4 = Player("4")
+    private val player5 = Player("5")
+    private val player6 = Player("6")
+    private val player7 = Player("7")
+    private val player8 = Player("8")
+    private val player9 = Player("9")
+    private val startingChips = 1000
+    private val fourPlayersGame = Game(listOf(player1, player2, player3, player4), startingChips)
 
     @Test
     fun `Game should not be constructed with no players`() {
-        assertThat({ Game(emptyList()) }, throws(equalTo(InvalidPlayersNumber(0))))
+        assertThat({ Game(emptyList(), startingChips) }, throws(equalTo(InvalidPlayersNumber(0))))
     }
 
     @Test
     fun `Game should not be constructed with one player`() {
-        assertThat({ Game(listOf(player1)) }, throws(equalTo(InvalidPlayersNumber(1))))
+        assertThat(
+            { Game(listOf(player1), startingChips) },
+            throws(equalTo(InvalidPlayersNumber(1)))
+        )
     }
 
     @Test
     fun `Game should be constructed with two players`() {
-        Game(listOf(player1, player2))
+        Game(listOf(player1, player2), startingChips)
     }
 
     @Test
@@ -46,13 +51,25 @@ class GameTest {
                     player8,
                     player9
                 )
+                , startingChips
             )
         }, throws(equalTo(InvalidPlayersNumber(9))))
     }
 
     @Test
     fun `Game should be constructed with eight players`() {
-        Game(listOf(player1, player2, player3, player4, player5, player6, player7, player8))
+        Game(
+            listOf(player1, player2, player3, player4, player5, player6, player7, player8),
+            startingChips
+        )
+    }
+
+    @Test
+    fun `Game should return all players as active after initialization`() {
+        assertThat(
+            fourPlayersGame.activePlayers,
+            equalTo(setOf(player1, player2, player3, player4))
+        )
     }
 
 }
