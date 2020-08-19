177c63df67889f63bac222522734cae7234de718
kn65op <199537+kn65op@users.noreply.github.com>
2020-08-19 19:21:09 +0200

    Two deals

--- androidApp/core/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/Deal.kt
+++ androidApp/core/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/Deal.kt
@@ -1,11 +1,47 @@
 package io.github.nadzwyczajnaGrupaRobocza.texaspoker.game
 
 class Deal(val players: List<DealPlayer>) {
+    private val deal = createDeal(players)
+
     init {
         assert(players.size > 1)
     }
 
+    private fun createDeal(players: List<DealPlayer>) = when (players.size) {
+        2 -> TwoPlayerDeal(players)
+        else -> ManyPlayersDeal(players)
+    }
+
+    private val betterId: Int
+        get() = bettingRound % players.size
+    private var bettingRound = deal.initialBettingRound
+
+    val nextBetter: String?
+        get() = players[betterId].uuid
+    val dealer: String
+        get() = deal.dealer
+    val smallBlind: String
+        get() = deal.smallBlind
+    val bigBlind: String
+        get() = deal.bigBlind
+}
+
+private abstract class DealImpl(val players: List<DealPlayer>) {
+    abstract val initialBettingRound: Int
     val dealer = players.last().uuid
-    val smallBlind = if (players.size == 2) dealer else players.first().uuid
-    val bigBlind = if (players.size == 2) players.first().uuid else players[1].uuid
+    abstract val smallBlind: String
+    abstract val bigBlind: String
+
+}
+
+private class TwoPlayerDeal(players: List<DealPlayer>) : DealImpl(players) {
+    override val initialBettingRound = 1
+    override val smallBlind = dealer
+    override val bigBlind = players.first().uuid
+}
+
+private class ManyPlayersDeal(players: List<DealPlayer>) : DealImpl(players) {
+    override val initialBettingRound = 2
+    override val smallBlind = players.first().uuid
+    override val bigBlind = players[1].uuid
 }
---
+++ androidApp/core/src/test/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/DealTest.kt
@@ -0,0 +1,37 @@
+package io.github.nadzwyczajnaGrupaRobocza.texaspoker.game
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.junit.Test
+
+open class DealTestData {
+    val player1 = DealPlayer("1", 1000)
+    val player2 = DealPlayer("2", 5000)
+}
+
+class DealTest : DealTestData() {
+    private val player3 = DealPlayer("3", 500)
+    private val player4 = DealPlayer("4", 100)
+    private val player5 = DealPlayer("5", 1000)
+    private val deal = Deal(listOf(player1, player2, player3, player4, player5))
+
+    @Test
+    fun `First better should be player 3`() {
+        assertThat(deal.nextBetter, equalTo(player3.uuid))
+    }
+}
+
+class TwoPlayerDealTest : DealTestData() {
+    private val deal = Deal(listOf(player1, player2))
+    private val 
+
+    @Test
+    fun `First better should be player 2`() {
+        assertThat(deal.nextBetter, equalTo(player2.uuid))
+    }
+
+    @Test
+    fun `Second better should be player 1`() {
+
+    }
+}
