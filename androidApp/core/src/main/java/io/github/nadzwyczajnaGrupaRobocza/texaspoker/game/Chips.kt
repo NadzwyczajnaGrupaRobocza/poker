48b1990555871becc22fbb848b6c259e9740f580
kn65op <199537+kn65op@users.noreply.github.com>
2020-08-23 14:39:37 +0200

    Add CHipsTest

--- androidApp/core/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/Chips.kt
+++ androidApp/core/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/Chips.kt
@@ -1,7 +1,11 @@
 package io.github.nadzwyczajnaGrupaRobocza.texaspoker.game
 
+import java.lang.Math.abs
+
 class Chips(initialAmount: Int) {
     fun change(change: ChipsChange) {
+        if (change.change < 0 && abs(change.change) > amount)
+            throw NotEnoughChips()
         changes.add(change.change)
     }
 
@@ -12,3 +16,5 @@ class Chips(initialAmount: Int) {
 }
 
 class ChipsChange(val change: Int)
+
+class NotEnoughChips : Throwable()
