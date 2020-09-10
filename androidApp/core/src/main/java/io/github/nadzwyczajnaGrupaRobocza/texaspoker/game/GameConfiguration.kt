e6a9a48f636bdd404c2556e5a7f5860235cc7a2e
kn65op <199537+kn65op@users.noreply.github.com>
2020-09-10 10:29:59 +0200

    Use data class

--- androidApp/core/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/GameConfiguration.kt
+++ androidApp/core/src/main/java/io/github/nadzwyczajnaGrupaRobocza/texaspoker/game/GameConfiguration.kt
@@ -1,3 +1,3 @@
 package io.github.nadzwyczajnaGrupaRobocza.texaspoker.game
 
-class GameConfiguration(val startingChips: Int, val blinds: Blinds)
+data class GameConfiguration(val startingChips: Int, val blinds: Blinds)
